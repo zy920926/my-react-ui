6e3a48807ec827715bf8eb992c55db4bae5a016f
songshuangfei <1462499968@qq.com>
2019-03-13 14:29:08 +0800

    优化水平滚动条

--- src/component/horizontal-scroll/horizontalscroll.jsx
+++ src/component/horizontal-scroll/horizontalscroll.jsx
@@ -14,27 +14,49 @@ class HorizontalScroll extends Component {
         this.touchMove = this.touchMove.bind(this);
         this.touchEnd = this.touchEnd.bind(this)
     }
+
+    isArrivedRight(){
+        const containerWidth = this.container.getBoundingClientRect().width;
+        const contentWidth = this.content.getBoundingClientRect().width;
+        const d = this.container.scrollLeft + containerWidth - contentWidth;
+        return Math.abs(d)< 0.5;// 靠近右边0.5内就算到达
+    }
+
+    isArrivedLeft(){
+        return this.container.scrollLeft === 0
+    }
+
     touchStart(){
         this.setState({duration:"0s"})//清除css变换过渡，防止拖动到边界出错
         //不能设置为0，否则setState并不会引起更新，估计是与react更新时对新的state判断造成的。所以我们设置成“0s”
     }
+
     touchMove(e){
-        if( this.container.scrollLeft !== 0 &&
-            this.container.scrollLeft + this.container.offsetWidth !== this.content.offsetWidth){
-            return;//滚动条未滑到两边，不作任何处理
+        if( !this.isArrivedLeft() && !this.isArrivedRight()){
+            return;// 滚动条未滑到两边，不作任何处理
         }
-        if(!this.startX){//滚动条到了两边，记录当前touch的x为起始位置
-            this.startX = e.touches[0].clientX
+
+        // 下面是滚动到了两边的逻辑（开始位移container），
+        if(!this.startX){
+            // touchMove第一次触发时记录当前touch的x为起始位置
+            this.startX = e.touches[0].clientX;
+            return;
         }
-        let offset = e.touches[0].clientX - this.startX;//计算手指滑动的位置
-        if(this.container.scrollLeft === 0 && offset < 0){
+
+        // 下面是第二次touchMove第二次及以后的逻辑
+        
+        let offset = e.touches[0].clientX - this.startX;// 计算手指滑动的位置
+
+        if(this.isArrivedLeft() && offset < 0){
+            // 滑倒最左时,接下来往右滑动不松开再往左滑动至起始位置左侧时
             offset = 0;
         }
 
-        if(this.container.scrollLeft + this.container.offsetWidth === this.content.offsetWidth && offset > 0){
+        if( this.isArrivedRight() && offset > 0){
             offset = 0
         }
-        this.setState({x:offset*0.2});//滚动条尽头，移动滚动条容器，限制距离为手指滑动的0.2倍
+        
+        this.setState({x:offset*0.1});// 滚动条尽头，移动滚动条容器，限制距离为手指滑动的0.2倍
     }
 
     touchEnd(){
